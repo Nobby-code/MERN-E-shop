4defcdd96e5c235f21d79a62b6abbbfef9b046fb
Nobby-code <norbertguda0@gmail.com>
2023-06-10 12:18:25 +0300

    redux implementation

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,25 +1,38 @@
 import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap';
 // import products from '../products'
 import Product from '../components/Product';
-import axios from 'axios';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { listProducts } from '../actions/productActions'
+// import axios from 'axios';
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
+    // const [products, setProducts] = useState([])
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
 
     useEffect(() => {
-        const fetchProducts = async () => {
+       /* const fetchProducts = async () => {
             const { data } = await axios.get('/api/products');
             setProducts(data);
         }
 
-        fetchProducts();
+        fetchProducts(); */
+        dispatch(listProducts())
 
-    }, [])
-    
+    }, [dispatch])
+     
     return (
         <>
             <h1>Latest Products</h1>
+            {loading ? 
+            <Loader />
+            : error ? 
+            <Message variant='danger'>{error}</Message>
+            :
             <Row>
                 {
                     products.map((product) => (
@@ -29,6 +42,7 @@ const HomeScreen = () => {
                     ))
                 }
             </Row>
+            }
         </>
     )
 }
